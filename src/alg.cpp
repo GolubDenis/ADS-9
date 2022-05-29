27a71dc580b25fce4da76837f1f7bb1364dd463e
GolubDenis <99288665+GolubDenis@users.noreply.github.com>
2022-05-29 23:07:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,32 +6,29 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-    BST<std::string> tree;
-    std::fstream file(filename);
-    if (!file) {
-        std::cout << "Ошибка файла" << std::endl;
-        return tree;
-    }
-    std::string str;
-    while (!file.eof()) {
-        char ch = file.get();
-        if (ch >= 'A' && ch <= 'Z') {
-            str += std::tolower(ch);
-        }
-        else 
-            if (ch >= 'a' && ch <= 'z') {
-            str += ch;
-        }
-        else {
-            if (str != "") {
-                tree.add(str);
-            }
-            str.clear();
-        }
-    }
-    if (str != "") {
-        tree.add(str);
-    }
-    file.close();
-    return tree;
+	BST<std::string> tree;
+	std::fstream file(filename);
+	if (!file) {
+		std::cout << "Ошибка файла" << std::endl;
+		return tree;
+	}
+	std::string str;
+	while (!file.eof()) {
+		char ch = file.get();
+		if (ch >= 'A' && ch <= 'Z') {
+			str += std::tolower(ch);
+		} else if (ch >= 'a' && ch <= 'z') {
+				str += ch;
+			}else {
+				if (str != "") {
+					tree.add(str);
+				}
+				str.clear();
+			}
+	}
+	if (str != "") {
+		tree.add(str);
+	}
+	file.close();
+	return tree;
 }
